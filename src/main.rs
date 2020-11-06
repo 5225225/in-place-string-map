d3acb62cf543db96277b4852962aaa65e6556166
5225225 <5225225@mailbox.org>
2020-11-06 18:28:39 +0000

    asdfsdafasdf

--- src/main.rs
+++ src/main.rs
@@ -16,46 +16,53 @@ impl std::ops::Drop for MapInPlace<'_> {
     }
 }
 
-fn map_in_place<'a>(s: &'a mut String) -> MapInPlace<'a> {
-    // I'm assuming that as_mut_vec has a similar requirement to str::as_bytes_mut
-    // Namely, from now until the MapInPlace reference ends, the string is allowed to be invalid
-    //
-    // So as long as we fix up the string to be valid UTF-8 before the end of every function that
-    // may have invalidated it, this is sound.
-    //
-    // Leaks are perfectly safe though! The only fixups we do in the Drop impl are to remove the
-    // unmapped portion.
-    unsafe {
-        MapInPlace {
-            buf: s.as_mut_vec(),
-            mapped_head: 0,
-            unmapped_head: 0,
-        }
-    }
-}
-
 #[derive(Debug)]
 struct NoCapacityError;
 
-impl MapInPlace<'_> {
-    fn mapped(&self) -> &str {
+impl<'a> MapInPlace<'a> {
+    /// Creates a new `MapInPlace`, used to do in-place string conversions without allocating a new
+    /// buffer.
+    ///
+    /// This can only be done if the conversion does not increase the length of the string (So
+    /// calling this on an empty string is useless).
+    pub fn new(s: &'a mut String) -> Self {
+        // I'm assuming that as_mut_vec has a similar requirement to str::as_bytes_mut
+        // Namely, from now until the MapInPlace reference ends, the string is allowed to be invalid
+        //
+        // So as long as we fix up the string to be valid UTF-8 before the end of every function that
+        // may have invalidated it, this is sound.
+        //
+        // Leaks are perfectly safe though! The only fixups we do in the Drop impl are to remove the
+        // unmapped portion.
+        unsafe {
+            MapInPlace {
+                buf: s.as_mut_vec(),
+                mapped_head: 0,
+                unmapped_head: 0,
+            }
+        }
+    }
+
+    /// Reads the mapped portion of the string
+    pub fn mapped(&self) -> &str {
         unsafe { std::str::from_utf8_unchecked(&self.buf[0..self.mapped_head]) }
     }
 
-    fn unmapped(&self) -> &str {
+    /// Reads the unmapped portion of the string
+    pub fn unmapped(&self) -> &str {
         unsafe { std::str::from_utf8_unchecked(&self.buf[self.unmapped_head..]) }
     }
 
     fn fix_utf8(&mut self) {
+        // This could be optimised, but for now this works.
         for ch in &mut self.buf[self.mapped_head..self.unmapped_head] {
             *ch = 0;
         }
     }
 
-    // this function isn't really useful by itself
-    // but it's an easy way to document the fact that self.buf must *always* be UTF-8 if user code
-    // can call methods on it.
-    fn all(&self) -> &str {
+    /// Reads the string as a while. The contents of the section not included in either [`mapped`]
+    /// or [`unmapped`] are unspecified, but the string as a whole is guaranteed to be valid UTF-8.
+    pub fn all(&self) -> &str {
         unsafe { std::str::from_utf8_unchecked(&self.buf[..]) }
     }
 
@@ -68,8 +75,9 @@ impl MapInPlace<'_> {
         Ok(())
     }
 
-    /// Pushes a character onto the end of the mapped portion
-    fn push(&mut self, ch: char) -> Result<(), NoCapacityError> {
+    /// Pushes a character onto the end of the mapped portion. Will return [`Err(NoCapacityError)`]
+    /// if there is no room.
+    pub fn push(&mut self, ch: char) -> Result<(), NoCapacityError> {
         let chlen = ch.len_utf8();
 
         if self.mapped_head + chlen > self.unmapped_head {
@@ -96,7 +104,7 @@ impl MapInPlace<'_> {
     }
 
     /// Pops a character from the start of the unmapped portion
-    fn pop(&mut self) -> Option<char> {
+    pub fn pop(&mut self) -> Option<char> {
         let ch = self.unmapped().chars().next()?;
 
         let l = ch.len_utf8();
@@ -109,12 +117,12 @@ impl MapInPlace<'_> {
     /// Pops a character from the start of the unmapped portion
     ///
     /// If `n` is 0 then will always return [`None`]
-    fn pop_chars(&mut self, n: usize) -> Option<&str> {
+    pub fn pop_chars(&mut self, n: usize) -> Option<&str> {
         if n == 0 {
             return None;
         }
 
-        let (idx, c) = self.unmapped().char_indices().nth(n)?;
+        let (idx, c) = self.unmapped().char_indices().nth(n-1)?;
 
         let to_take = idx + c.len_utf8();
 
@@ -126,29 +134,66 @@ impl MapInPlace<'_> {
     }
 }
 
-fn main() {
-    let mut s = "abc%64%65fg".to_string();
+fn decode_percent(s: &mut String) {
+    let mut m = MapInPlace::new(s);
 
-    {
-        let mut m = map_in_place(&mut s);
-
-        while let Some(c) = m.pop() {
-            match c {
-                '%' => {
-                    let num = m.pop_chars(2).expect("not enough chars");
-                    let n = u8::from_str_radix(num, 16).expect("invalid hex");
-                    m.push(n as char).expect("no more capacity");
-                }
-                _ => {
-                    m.push(c).expect("no more capacity");
-                }
+    while let Some(c) = m.pop() {
+        match c {
+            '%' => {
+                let num = m.pop_chars(2).expect("not enough chars");
+                let n = u8::from_str_radix(num, 16).expect("invalid hex");
+                m.push(n as char).expect("no more capacity");
+            }
+            _ => {
+                m.push(c).expect("no more capacity");
             }
         }
     }
 }
 
+fn decode_c(s: &mut String) {
+    let mut m = MapInPlace::new(s);
+
+    while let Some(c) = m.pop() {
+        match c {
+            '\\' => {
+                match m.pop() {
+                    Some('a') => m.push('\x07'),
+                    Some('b') => m.push('\x08'),
+                    Some('e') => m.push('\x1b'),
+                    Some('f') => m.push('\x0c'),
+                    Some('n') => m.push('\x0a'),
+                    Some('r') => m.push('\x0d'),
+                    Some('t') => m.push('\x09'),
+                    Some('v') => m.push('\x0b'),
+                    Some('\\') => m.push('\x5c'),
+                    Some('\'') => m.push('\x27'),
+                    Some('\"') => m.push('\x22'),
+                    Some('?') => m.push('\x3f'),
+                    Some(num @ '0'..='9') => {
+
+                    }
+                    None => break,
+                };
+            }
+
+            _ => {
+
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut percent = "abc%64%65fg".to_string();
+    decode_percent(&mut percent);
+    assert_eq!(percent, "abcdefg");
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[derive(proptest_derive::Arbitrary, Debug)]
     enum TestOp {
         Pop,
@@ -166,7 +211,7 @@ mod tests {
         fn always_valid(mut start: String, ops: Vec<TestOp>) {
             let mut model = Model { unmapped: start.clone(), mapped: String::new()};
 
-            let mut m = map_in_place(&mut start);
+            let mut m = MapInPlace::new(&mut start);
 
             for o in ops {
                 match o {
@@ -207,36 +252,5 @@ mod tests {
                 assert_eq!(m.mapped(), model.mapped, "mapped did not match");
             }
         }
-    }
-
-    use super::*;
-    const ONEWIDE: char = 'X';
-    const TWOWIDE: char = '£';
-    const THREEWIDE: char = '\u{2713}';
-    const FOURWIDE: char = '\u{10102}';
-
-    #[test]
-    fn overlapping_bytes() {
-        let mut s = "££".to_string();
-
-        let mut m = map_in_place(&mut s);
-
-        m.pop().unwrap();
-        m.push('$');
-
-        assert_eq!(m.mapped(), "$");
-        assert_eq!(m.unmapped(), "£");
-
-        m.push('$');
-
-        assert_eq!(m.mapped(), "$$");
-        assert_eq!(m.unmapped(), "£");
-    }
-
-    #[test]
-    fn complex_overlapping_bytes() {
-        let mut s = String::new();
-        s.extend(&[ONEWIDE, FOURWIDE, FOURWIDE]);
-        let mut m = map_in_place(&mut s);
     }
 }
